209af1f5c3e2d7f47902a0795a5a740dbc827062
patej17 <patej17@mcmaster.ca>
2024-01-26 15:24:28 -0500

    started the ciruclar buffer implementation

--- inc/CircularQueue.h
+++ inc/CircularQueue.h
@@ -16,34 +16,46 @@
 
 typedef struct
 {
-    message_t* circular_buffer;
+    void** circular_buffer;
+    uint8_t* occupied;
     uint8_t read_index;
-    uint8_t writeIndex;
+    uint8_t write_index;
     uint8_t size;
     uint8_t max_size;
-    pthread_mutex_t mutex;
+    size_t data_size;
+    pthread_mutex_t* mutex;
 } circular_queue_handle;
 
 /**
  * @brief Initialize a circular queue
  *
  * @param handle The handle to the queue
- * @param size The maxinum length of the queue
+ * @param data_size The size of the data type
+ * @param length The maxinum length of the queue
  */
-void queue_init(circular_queue_handle* handle, uint8_t size);
+void queue_init(circular_queue_handle* handle, size_t data_size, uint8_t length);
+
+/**
+ * @brief Destroy the given queue
+ *
+ * @param handle The handle of the queue
+ */
+void queue_destroy(circular_queue_handle* handle);
 
 /**
  * @brief Enqueue a message to the end of the given queue
  *
  * @param handle The handle of the queue
- * @param msg The message to add to the queue
+ * @param data The data to add to the queue
  */
-void queue_enqueue(circular_queue_handle* handle, message_t* msg);
+void queue_get_next_open(circular_queue_handle* handle, void* data);
 
 /**
  * @brief Dequeue a given message from the queue
  *
  * @param handle The handle of the queue
- * @param msg The message to remove from the queue
+ * @param data The data to remove from the queue
  */
-void queue_deqeueue(circular_queue_handle* handle, message_t* msg);
+void queue_deqeueue(circular_queue_handle* handle, void* data);
+
+uint8_t find_next_open(circular_queue_handle* handle);
