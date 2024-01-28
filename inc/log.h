8370938781f7f23debc40ec5afcf5dace60be033
patej17 <patej17@mcmaster.ca>
2024-01-28 01:54:05 -0500

    created a simple log service

---
+++ inc/log.h
@@ -0,0 +1,32 @@
+/**
+ * @file log.h
+ * @author Jay Patel
+ * @brief Simple Log Service
+ * @version 0.1
+ * @date 2024-01-28
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include <stdio.h>
+
+#define ENABLE_ERROR_LOGS 1
+#define ENABLE_INFO_LOGS 0
+
+#ifndef LOG_H
+#define LOG_H
+
+#if ENABLE_ERROR_LOGS == 1
+#define LOGE(...)  printf("ERROR: " __VA_ARGS__);
+#else
+#define LOGE(...) // do nothing
+#endif
+
+#if ENABLE_INFO_LOGS == 1
+#define LOGI(...)  printf("INFO: " __VA_ARGS__);
+#else
+#define LOGI(...) // do nothing
+#endif
+
+#endif
